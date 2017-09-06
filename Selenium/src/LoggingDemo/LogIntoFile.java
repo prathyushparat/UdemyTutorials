21d344e9077b9a19bd65b900fa5933b30e900ef1
prathyush parat <prathyushparat@gmail.com>
2017-09-06 16:31:29 +0530

    Logging tutorials checking in

--- Selenium/src/LoggingDemo/LogIntoFile.java
+++ Selenium/src/LoggingDemo/LogIntoFile.java
@@ -3,10 +3,13 @@ package LoggingDemo;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class LoggingConsole {
-	private static final Logger log = LogManager.getLogger(LoggingConsole.class.getName());
+public class LogIntoFile {
+	private static final Logger log = LogManager.getLogger(LogIntoFile.class.getName());
 
 	public static void main(String[] args) {
+		log.trace("Trace logs");
+		log.info("Info logs");
+		log.warn("Warn logs");
 		log.debug("Debug logs");
 		log.error("Error logs");
 		log.fatal("Fatal logs");
