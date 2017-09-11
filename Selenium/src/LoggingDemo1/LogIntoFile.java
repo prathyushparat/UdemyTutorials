a969ed3af862668581b964663679e558072b9e9e
prathyush parat <prathyushparat@gmail.com>
2017-09-11 11:32:37 +0530

    committing package Selenium/src/LoggingDemo1

---
+++ Selenium/src/LoggingDemo1/LogIntoFile.java
@@ -0,0 +1,19 @@
+package LoggingDemo1;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class LogIntoFile {
+	private static final Logger log = LogManager.getLogger(LogIntoFile.class.getName());
+
+	public static void main(String[] args) {
+		log.trace("Trace logs");
+		log.info("Info logs");
+		log.warn("Warn logs");
+		log.debug("Debug logs");
+		log.error("Error logs");
+		log.fatal("Fatal logs");
+
+	}
+
+}
